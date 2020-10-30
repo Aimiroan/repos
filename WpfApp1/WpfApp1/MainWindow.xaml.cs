1de5251b8c0e172f2090338180a65dc0d71d6d80
Michael van Dijk <aimiroan@hotmail.com>
2020-10-30 16:31:10 +0100

    Changes

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -21,23 +21,75 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         readonly SolidColorBrush b = new SolidColorBrush();
-        Image mainImage = new Image();
+        List<KeyValuePair<Image, bool>> problems = new List<KeyValuePair<Image, bool>>();
+        bool view = false;
 
         public MainWindow()
         {
             InitializeComponent();
+            InitializeProblems();
+
+            var value = problems.Where(kvp => kvp.Key == mainImage2).Select(kvp => kvp.Value).ToString();
+            Console.WriteLine("Test: " + value + " : Test end");
+
+            SetAllHidden();
+            mainImage2.Visibility = Visibility.Hidden;
+        }
+
+        protected void InitializeProblems()
+        {
+            problems.Add(new KeyValuePair<Image, bool>(inletValveA, false));
+            problems.Add(new KeyValuePair<Image, bool>(inletValveB, true));
+            problems.Add(new KeyValuePair<Image, bool>(outletValveA, false));
+            problems.Add(new KeyValuePair<Image, bool>(outletValveB, false));
+            problems.Add(new KeyValuePair<Image, bool>(primSealA, true));
+            problems.Add(new KeyValuePair<Image, bool>(primSealB, true));
+            problems.Add(new KeyValuePair<Image, bool>(notSure, true));
+            problems.Add(new KeyValuePair<Image, bool>(mainImage2, true));
         }
 
         private void But1_Click(object sender, RoutedEventArgs e)
         {
-            b.Color = Color.FromRgb(0, 0, 0);
-            Rec1.Fill = b;
+            SetAllHidden();
         }
 
         private void But2_Click(object sender, RoutedEventArgs e)
         {
-            b.Color = Color.FromRgb(255, 0, 0);
-            Rec1.Fill = b;
+            ShowProblems();
+        }
+        
+        private void But3_Click(object sender, RoutedEventArgs e)
+        {
+            if (view == false)
+            {
+                SetAllHidden();
+                mainImage.Visibility = Visibility.Hidden;
+                mainImage2.Visibility = Visibility.Visible;
+                view = true;
+            } else
+            {
+                ShowProblems();
+                mainImage.Visibility = Visibility.Visible;
+                mainImage2.Visibility = Visibility.Hidden;
+                view = false;
+            }
+        }
+
+        protected void SetAllHidden()
+        {
+            foreach (KeyValuePair<Image, bool> img in problems)
+            {
+                img.Key.Visibility = Visibility.Hidden;
+            }
+        }
+
+        protected void ShowProblems()
+        {
+            foreach (KeyValuePair<Image, bool> img in problems)
+            {
+                if (img.Value == true & img.Key != mainImage2)
+                    img.Key.Visibility = Visibility.Visible;
+            }
         }
     }
 }
